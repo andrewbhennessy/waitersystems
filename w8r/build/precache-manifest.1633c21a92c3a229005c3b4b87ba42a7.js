060082344d02d6f254ba4a70847a504d794354a6
Michael Burke <michaelb@Michaels-MacBook-Pro-6.local>
2019-10-11 19:29:54 -0400

    Overriding gitignore

---
+++ w8r/build/precache-manifest.1633c21a92c3a229005c3b4b87ba42a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f74f65ed3dda4b2a799c2f373adfe0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61674ff848fc4c862532",
+    "url": "/static/css/2.9df936aa.chunk.css"
+  },
+  {
+    "revision": "a575302e432f9e3bba31",
+    "url": "/static/css/main.45389d2e.chunk.css"
+  },
+  {
+    "revision": "61674ff848fc4c862532",
+    "url": "/static/js/2.e2bae24f.chunk.js"
+  },
+  {
+    "revision": "a575302e432f9e3bba31",
+    "url": "/static/js/main.1b0d630b.chunk.js"
+  },
+  {
+    "revision": "8c1b052954cead8b0560",
+    "url": "/static/js/runtime-main.d8133a65.js"
+  }
+]);
